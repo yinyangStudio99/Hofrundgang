a91fd463d699c2799b9bbd3bd19f74fb0a5b4992
yinyangStudio99 <yinyang_studio@hotmail.com>
2023-01-26 19:59:06 +0100

    upload

---
+++ data.js
@@ -0,0 +1,569 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hofrundgang_k01",
+      "name": "Hofrundgang_K01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007534946317139912,
+          "pitch": 0.0018239812787328447,
+          "rotation": 0,
+          "target": "1-hofrundgang_k02"
+        },
+        {
+          "yaw": 1.54357969621927,
+          "pitch": 0.0007161452450468175,
+          "rotation": 0,
+          "target": "2-hofrundgang_k03"
+        },
+        {
+          "yaw": 2.9175608093752334,
+          "pitch": 0.006311320376200413,
+          "rotation": 0,
+          "target": "7-hofrundgang_k08"
+        },
+        {
+          "yaw": -2.9246951844495648,
+          "pitch": 0.00289785879401272,
+          "rotation": 0,
+          "target": "8-hofrundgang_k09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hofrundgang_k02",
+      "name": "Hofrundgang_K02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008264791162041263,
+          "pitch": 0.0002617407483267442,
+          "rotation": 0,
+          "target": "0-hofrundgang_k01"
+        },
+        {
+          "yaw": -0.8362160118729403,
+          "pitch": 0.007706696423269932,
+          "rotation": 0,
+          "target": "2-hofrundgang_k03"
+        },
+        {
+          "yaw": -1.1963350726796627,
+          "pitch": -0.0021800458696539238,
+          "rotation": 0,
+          "target": "3-hofrundgang_k04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hofrundgang_k03",
+      "name": "Hofrundgang_K03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.524893077805224,
+          "pitch": 0.005333119758724081,
+          "rotation": 0,
+          "target": "0-hofrundgang_k01"
+        },
+        {
+          "yaw": -0.9332076448239786,
+          "pitch": 0.004987628393854138,
+          "rotation": 0,
+          "target": "1-hofrundgang_k02"
+        },
+        {
+          "yaw": 1.5751822597678942,
+          "pitch": -0.002897886094842761,
+          "rotation": 0,
+          "target": "3-hofrundgang_k04"
+        },
+        {
+          "yaw": 2.93688071718738,
+          "pitch": -0.009030982345549887,
+          "rotation": 0,
+          "target": "4-hofrundgang_k05"
+        },
+        {
+          "yaw": -2.395730956367382,
+          "pitch": -0.010192781273797635,
+          "rotation": 0,
+          "target": "7-hofrundgang_k08"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hofrundgang_k04",
+      "name": "Hofrundgang_K04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.409044733790177,
+          "pitch": 0.00433533658463503,
+          "rotation": 0,
+          "target": "4-hofrundgang_k05"
+        },
+        {
+          "yaw": -1.5469805987298173,
+          "pitch": 0.003735641519662991,
+          "rotation": 0,
+          "target": "2-hofrundgang_k03"
+        },
+        {
+          "yaw": -1.9736728057581026,
+          "pitch": -0.0030944170623623535,
+          "rotation": 0,
+          "target": "7-hofrundgang_k08"
+        },
+        {
+          "yaw": -1.2556800982731886,
+          "pitch": 0.011930997020549228,
+          "rotation": 0,
+          "target": "1-hofrundgang_k02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hofrundgang_k05",
+      "name": "Hofrundgang_K05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6620227240324272,
+          "pitch": 0.005822350148743993,
+          "rotation": 0,
+          "target": "3-hofrundgang_k04"
+        },
+        {
+          "yaw": 0.1256028486105052,
+          "pitch": 0.012034913631467248,
+          "rotation": 0,
+          "target": "2-hofrundgang_k03"
+        },
+        {
+          "yaw": -0.8851521936107272,
+          "pitch": 0.006544170727234899,
+          "rotation": 0,
+          "target": "0-hofrundgang_k01"
+        },
+        {
+          "yaw": -0.6514182954585639,
+          "pitch": 0.0044406020031324545,
+          "rotation": 0,
+          "target": "1-hofrundgang_k02"
+        },
+        {
+          "yaw": -2.856649212176835,
+          "pitch": -0.01134937374082412,
+          "rotation": 0,
+          "target": "5-hofrundgang_k06"
+        },
+        {
+          "yaw": -1.2487935042076135,
+          "pitch": 0.010155979941110616,
+          "rotation": 0,
+          "target": "7-hofrundgang_k08"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hofrundgang_k06",
+      "name": "Hofrundgang_K06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2753491656655189,
+          "pitch": -0.00270397082004159,
+          "rotation": 0,
+          "target": "4-hofrundgang_k05"
+        },
+        {
+          "yaw": -0.6559515143437338,
+          "pitch": -0.004919139494880653,
+          "rotation": 0,
+          "target": "6-hofrundgang_k07"
+        },
+        {
+          "yaw": -1.5022850373875816,
+          "pitch": 0.005873602058722227,
+          "rotation": 0,
+          "target": "8-hofrundgang_k09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-hofrundgang_k07",
+      "name": "Hofrundgang_K07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1128888751760293,
+          "pitch": 0.009387122603246922,
+          "rotation": 0,
+          "target": "7-hofrundgang_k08"
+        },
+        {
+          "yaw": -0.06880630474392468,
+          "pitch": 0.012438159899556211,
+          "rotation": 0,
+          "target": "1-hofrundgang_k02"
+        },
+        {
+          "yaw": 0.6630470731236038,
+          "pitch": 0.01240902778008568,
+          "rotation": 0,
+          "target": "2-hofrundgang_k03"
+        },
+        {
+          "yaw": 0.9941056604912433,
+          "pitch": 0.0027271061283862252,
+          "rotation": 0,
+          "target": "3-hofrundgang_k04"
+        },
+        {
+          "yaw": -2.645396435495,
+          "pitch": -0.0005458022740292279,
+          "rotation": 0,
+          "target": "8-hofrundgang_k09"
+        },
+        {
+          "yaw": 2.5709337953347564,
+          "pitch": -0.0017312458219436166,
+          "rotation": 0,
+          "target": "5-hofrundgang_k06"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-hofrundgang_k08",
+      "name": "Hofrundgang_K08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8110323836543385,
+          "pitch": 0.01202627570686765,
+          "rotation": 0,
+          "target": "4-hofrundgang_k05"
+        },
+        {
+          "yaw": 1.245291693431124,
+          "pitch": 0.010084593290411448,
+          "rotation": 0,
+          "target": "3-hofrundgang_k04"
+        },
+        {
+          "yaw": 0.8002601558726834,
+          "pitch": 0.01650013713993914,
+          "rotation": 0,
+          "target": "2-hofrundgang_k03"
+        },
+        {
+          "yaw": -0.10914102804947134,
+          "pitch": 0.018115275674695397,
+          "rotation": 0,
+          "target": "0-hofrundgang_k01"
+        },
+        {
+          "yaw": 2.8197924011534425,
+          "pitch": -0.007873449821715894,
+          "rotation": 0,
+          "target": "6-hofrundgang_k07"
+        },
+        {
+          "yaw": -2.774442942635165,
+          "pitch": -0.015344714046367613,
+          "rotation": 0,
+          "target": "8-hofrundgang_k09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-hofrundgang_k09",
+      "name": "Hofrundgang_K09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6444983964137325,
+          "pitch": 0.0011929896845970234,
+          "rotation": 0,
+          "target": "5-hofrundgang_k06"
+        },
+        {
+          "yaw": 0.4324537782115989,
+          "pitch": 0.015256021023713373,
+          "rotation": 0,
+          "target": "6-hofrundgang_k07"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
